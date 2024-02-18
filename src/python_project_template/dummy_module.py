cec9825ff5b92b3f2541a6e67b9d7e9818d2dced
Matias Chomali <matias.chomali.dev@gmail.com>
2024-02-18 01:06:56 -0800

    add ruff

--- src/python_project_template/dummy_module.py
+++ src/python_project_template/dummy_module.py
@@ -1,2 +1,2 @@
 def dummy_func() -> int:
-    return 1;
+    return 1
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,5 +1,6 @@
 from src.python_project_template.dummy_module import dummy_func
 
+
 def test_dummy():
     result: int = dummy_func()
     assert result == 1
